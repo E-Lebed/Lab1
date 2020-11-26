b015c12b41fecfe5ebf578dbf0170504eeccc967
Liza <lebedaple@gmail.com>
2020-11-27 01:38:08 +0300

    Lab work

---
+++ Lab/src/test/java/sutist012m/lebed/Lab/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sutist012m.lebed.Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
